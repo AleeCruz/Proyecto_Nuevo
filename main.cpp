0f62c64839fab5a8ff5dd095453e71f2098bf039
Alexander <acruz@fi.uba.ar>
2024-11-15 19:03:37 -0300

    Agregue unas nuevas lineas de codigo

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
-#include<stdio.h>
+#include<iostream>
+
+using namespace std;
 
 int main(){
     int numero1 =1;
     int numero2 = 2;
+    int resultado = numero1 + numero2;
 
+    cout<<"El resultado de la suma es: "<<resultado;
 
     return 0;
 }
